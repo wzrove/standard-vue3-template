cb67d476eb25fcbe70990e04f1a91469fe4c681d
wu.zhuo <wzrove@outlook.com>
2022-05-23 10:53:21 +0800

    feat: add windcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
+import WindiCSS  from 'vite-plugin-windicss'
 
 function pathResolve(dir: string) {
   return resolve(process.cwd(), '.', dir);
 }
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),WindiCSS()],
   resolve: {
     alias: [
       {
@@ -16,4 +17,7 @@ export default defineConfig({
       },
     ],
   },
+  server: {
+    port:8081
+  }
 });
