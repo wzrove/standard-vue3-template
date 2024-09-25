621502357d9e055a073b574d4b6c0b16769288da
wuzhuo <wuzhuo@redcoast.info>
2024-09-25 18:52:36 +0800

    feat: 更换biomejs作为校验工具

--- .husky/lintstagedrc.js
+++ .husky/lintstagedrc.js
@@ -1,8 +1,5 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
-  '{!(package)*.json,*.code-snippets,}': ['prettier --write--parser json'],
-  'package.json': ['prettier --write'],
-  '*.vue': ['eslint --fix', 'prettier --write'],
-  '*.{scss,less,styl,html}': ['prettier --write'],
-  '*.md': ['prettier --write'],
+	"*": [
+		"biome check --fix --unsafe  --no-errors-on-unmatched --files-ignore-unknown=true", // Check formatting and lint
+	],
 };
